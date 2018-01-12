e5f6ac194a3d2b1ff677e3cf576e9393686ea34e
Hiroki Noda <kubo39@gmail.com>
2018-01-12 14:30:46 +0900

    Update tcpserver example

--- examples/tcpserver/source/app.d
+++ examples/tcpserver/source/app.d
@@ -8,14 +8,59 @@ void zeroHandler(const Request* req, Response* res)
     switch (req.pdu.functionCode)
     {
     case FunctionCode.ReadCoils:
+        res.pdu.data = [
+            0x1,  // Byte count
+            0x0,  // Coil Status
+            ];
+        break;
     case FunctionCode.ReadDiscreteInputs:
+        res.pdu.data = [
+            0x1,  // Byte count
+            0x0,  // Input status
+            ];
+        break;
     case FunctionCode.ReadHoldingRegisters:
+        res.pdu.data = [
+            0x1,  // Byte count
+            0x0,  // Register value
+            ];
+        break;
     case FunctionCode.ReadInputRegisters:
+        res.pdu.data = [
+            0x2,     // Byte count
+            0x0, 0x0 // Input Registers
+            ];
+        break;
     case FunctionCode.WriteSingleCoil:
+        res.pdu.data = [
+            0x0, 0x0,  // Output Address
+            0x0, 0x0,  // Output Value
+            ];
+        break;
     case FunctionCode.WriteSingleRegister:
+        res.pdu.data = [
+            0x0, 0x0,  // Register Address
+            0x0, 0x0,  // Register Value
+            ];
+        break;
     case FunctionCode.WriteMultipleCoils:
+        res.pdu.data = [
+            0x0, 0x0,  // Starting Address
+            0x0, 0x0,  // Quantity of Output
+            ];
+        break;
     case FunctionCode.WriteMultipleRegisters:
+        res.pdu.data = [
+            0x0, 0x0,  // Starting Address
+            0x0, 0x0,  // Quantity of Registers
+            ];
+        break;
     case FunctionCode.ReadWriteMultipleRegisters:
+        res.pdu.data = [
+            0x2,       // Byte Count
+            0x0, 0x0,  // Read Registers value
+            ];
+        break;
     default:
         assert(false);
     }

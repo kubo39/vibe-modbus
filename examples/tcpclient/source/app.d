94f7d7ea8dbb365664eaaebab5239d42869717a4
Hiroki Noda <kubo39@gmail.com>
2018-01-16 03:10:58 +0900

    Add request for WriteSingleRegisters in client example

--- examples/tcpclient/source/app.d
+++ examples/tcpclient/source/app.d
@@ -4,10 +4,28 @@ import vibemodbus.tcp.client;
 void main()
 {
     auto client = Client("localhost", 40960);
-    writeln("Calling ReadHolding Registers");
+
+    writeln("Calling Read Holding Registers");
     auto res = client.readHoldingRegisters(0x1000, 7);
-    writeln("MBAP Header: ", res.header);
+    writeln("MBAP Header: ");
+    writeln("  transaction id: ", res.header.transactionId);
+    writeln("  protocol id: ", res.header.protocolId);
+    writeln("  length: ", res.header.length);
+    writeln("  unit id: ", res.header.unitId);
     writeln("FunctionCode: ", cast(FunctionCode) res.functionCode);
     writeln("Byte Count: ", res.byteCount);
     writeln("Register Value: ", res.registerValue);
+
+    writeln("\n");
+
+    writeln("Calling Write Single Registers");
+    auto res2 = client.writeSingleRegister(0x1000, 20);
+    writeln("MBAP Header: ");
+    writeln("  transaction id: ", res2.header.transactionId);
+    writeln("  protocol id: ", res2.header.protocolId);
+    writeln("  length: ", res2.header.length);
+    writeln("  unit id: ", res2.header.unitId);
+    writeln("FunctionCode: ", cast(FunctionCode) res2.functionCode);
+    writeln("Register Address: ", res2.registerAddress);
+    writeln("Register Value: ", res2.registerValue);
 }

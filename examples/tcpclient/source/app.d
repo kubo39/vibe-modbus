99c88b8fc9855693e173643832617da9159a44e5
Hiroki Noda <kubo39@gmail.com>
2018-01-16 15:35:42 +0900

    Refactor client example

--- examples/tcpclient/source/app.d
+++ examples/tcpclient/source/app.d
@@ -1,44 +1,55 @@
 import std.stdio;
 import vibemodbus.tcp.client;
 
-void main()
+void callReadHoldingRegisters(Client client)
 {
-    auto client = Client("localhost", 40960);
-
-    writeln("Calling Read Holding Registers");
+    writeln("Calling Read Holding Registers...");
     auto res = client.readHoldingRegisters(0x1000, 7);
     writeln("MBAP Header: ");
     writeln("  transaction id: ", res.header.transactionId);
     writeln("  protocol id: ", res.header.protocolId);
     writeln("  length: ", res.header.length);
     writeln("  unit id: ", res.header.unitId);
-    writeln("FunctionCode: ", cast(FunctionCode) res.functionCode);
-    writeln("Byte Count: ", res.byteCount);
-    writeln("Register Value: ", res.registerValue);
+    writefln("FunctionCode: %#x", cast(FunctionCode) res.functionCode);
+    writefln("Byte Count: %#x", res.byteCount);
+    writefln("Register Value: [%(%#x %)]", res.registerValue);
+}
 
-    writeln("\n");
-
-    writeln("Calling Write Single Register");
-    auto res2 = client.writeSingleRegister(0x1000, 20);
+void callWriteSingleRegister(Client client)
+{
+    writeln("Calling Write Single Register...");
+    auto res = client.writeSingleRegister(0x1000, 20);
     writeln("MBAP Header: ");
-    writeln("  transaction id: ", res2.header.transactionId);
-    writeln("  protocol id: ", res2.header.protocolId);
-    writeln("  length: ", res2.header.length);
-    writeln("  unit id: ", res2.header.unitId);
-    writeln("FunctionCode: ", cast(FunctionCode) res2.functionCode);
-    writeln("Register Address: ", res2.registerAddress);
-    writeln("Register Value: ", res2.registerValue);
+    writeln("  transaction id: ", res.header.transactionId);
+    writeln("  protocol id: ", res.header.protocolId);
+    writeln("  length: ", res.header.length);
+    writeln("  unit id: ", res.header.unitId);
+    writefln("FunctionCode: %#x", cast(FunctionCode) res.functionCode);
+    writefln("Register Address: %#x", res.registerAddress);
+    writefln("Register Value: %#x", res.registerValue);
+}
 
-    writeln("\n");
-
-    writeln("Calling Write Multiple Registers");
-    auto res3 = client.writeMultipleRegisters(0x1000, 0x20, [0x10, 0x10]);
+void callWriteMultipleRegisters(Client client)
+{
+    writeln("Calling Write Multiple Registers...");
+    auto res = client.writeMultipleRegisters(0x1000, 0x20, [0x10, 0x10]);
     writeln("MBAP Header: ");
-    writeln("  transaction id: ", res3.header.transactionId);
-    writeln("  protocol id: ", res3.header.protocolId);
-    writeln("  length: ", res3.header.length);
-    writeln("  unit id: ", res3.header.unitId);
-    writeln("FunctionCode: ", cast(FunctionCode) res3.functionCode);
-    writeln("Starting Address: ", res3.startingAddress);
-    writeln("Quantity of Registers: ", res3.quantityOfRegisters);
+    writeln("  transaction id: ", res.header.transactionId);
+    writeln("  protocol id: ", res.header.protocolId);
+    writeln("  length: ", res.header.length);
+    writeln("  unit id: ", res.header.unitId);
+    writefln("FunctionCode: %#x", cast(FunctionCode) res.functionCode);
+    writefln("Starting Address: %#x", res.startingAddress);
+    writefln("Quantity of Registers: %#x", res.quantityOfRegisters);
+}
+
+void main()
+{
+    auto client = Client("localhost", 40960);
+
+    client.callReadHoldingRegisters();
+    writeln("\n");
+    client.callWriteSingleRegister();
+    writeln("\n");
+    client.callWriteMultipleRegisters();
 }
